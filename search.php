94dc0875db25e7c237930edad074eeb9ddf208f0
Nick Rucci <ld.tech@outlook.com>
2016-03-12 13:12:36 -0700

    Initial commit

---
+++ search.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search Results</title>
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no"/>
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/search.css"/>
+    <link href='//fonts.googleapis.com/css?family=Ubuntu:400,700' rel='stylesheet'>
+    <link href='//fonts.googleapis.com/css?family=Play:400,700' rel='stylesheet'>
+    <link rel='stylesheet' href='css/font-awesome.css'>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery-migrate-1.2.1.js"></script>
+
+    <!--[if lt IE 9]>
+    <html class="lt-ie9">
+    <div style=' clear: both; text-align:center; position: relative;'>
+        <a href="http://windows.microsoft.com/en-US/internet-explorer/..">
+            <img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820"
+                 alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."/>
+        </a>
+    </div>
+    <script src="js/html5shiv.js"></script>    
+  	<script src='js/selectivizr-min.js'></script>
+    <![endif]-->
+
+    <script src='js/device.min.js'></script>
+</head>
+
+<body>
+<div class="page">
+    <!--========================================================
+                              HEADER
+    =========================================================-->
+    <header class="bg-image-1">
+        <div class="top-part">
+            <div class="container">
+                <ul class="inline-list top-menu">
+                    <li><a href="#">sitemap</a></li>
+                    <li><a href="#">faq</a></li>
+                </ul>
+                <ul class="inline-list text-right">
+                    <li><a class="icon icon-md icon-secondary fa-facebook" href="#"></a></li>
+                    <li><a class="icon icon-md icon-secondary fa-google-plus" href="#"></a></li>
+                    <li><a class="icon icon-md icon-secondary fa-rss" href="#"></a></li>
+                    <li><a class="icon icon-md icon-secondary fa-pinterest" href="#"></a></li>
+                    <li><a class="icon icon-md icon-secondary fa-linkedin" href="#"></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="container">
+            <div class="rd-navbar-brand">
+                <span class="bsh"></span>
+                <h1 class="rd-navbar-brand_name">
+                    <a href="./">mariner</a>
+                </h1>
+                <p class="rd-navbar-brand_slogan">marine construction</p>
+            </div>
+            <section class="well-sm text-sm-right">
+                <dl>
+                    <dt>Free phone:</dt>
+                    <dd><a class="bold" href="callto:#">+ 8 800 757 43 92</a></dd>
+                </dl>
+            </section>
+        </div>
+        <section class="well-lg bg-image bg-image-2">
+
+        </section>
+        <div id="stuck_container" class="stuck_container">
+            <div class="container relative">
+                <nav class="nav">
+                    <ul class="sf-menu" data-type="navbar">
+                        <li >
+                            <a href="./">Home</a>
+                        </li>
+                        <li >
+                            <a href="index-1.html">About</a>
+                            <ul>
+                                <li>
+                                    <a href="#">History</a>
+                                </li>
+                                <li>
+                                    <a href="#">News</a>
+                                    <ul>
+                                        <li>
+                                            <a href="#">History</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">News</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Testimolials</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#">Testimonials</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="index-2.html">Services</a>
+                        </li>
+                        <li>
+                            <a href="index-3.html">Projects</a>
+                        </li>
+                        <li>
+                            <a href="index-4.html">Contacts</a>
+                        </li>
+                    </ul>
+                    <form class="search-form" action="search.php" method="GET" accept-charset="utf-8">
+                        <label class="search-form_label">
+                            <input class="search-form_input" type="text" name="s" autocomplete="off"/>
+                            <span class="search-form_liveout"></span>
+                        </label>
+                        <button class="search-form_submit" type="submit"></button>
+                    </form>
+                </nav>
+            </div>
+        </div>
+
+
+    </header>
+
+    <!--========================================================
+                              CONTENT
+    =========================================================-->
+    <section id="content" class="content well-xl">
+        <div class="container ">
+            <h4>Search Results</h4>
+            <div id="search-results"></div>
+        </div>
+    </section>
+
+    <!--========================================================
+                              FOOTER
+    =========================================================-->
+   <footer class="text-center">
+     <section class="bg-primary well-xs banner text-md-left">
+       <div class="container">
+         <div class="row">
+             <div class="col-md-9 wow fadeInLeft">
+               <div class="banner-name">
+                 <p class="big">View our events calendar</p>
+               </div>
+             </div>
+             <div class="col-md-3 wow fadeInRight">
+               <time class="big" datetime="2014-01-01">2014</time>
+               <span class="icon icon-primary-img"></span>
+           </div>
+         </div>
+       </div>
+     </section>
+     <section class="well text-md-left bg-image-1">
+       <div class="container">
+         <div class="row flow-offset wow fadeInUp">
+           <div class="col-md-3 col-sm-6 pull-lg-right">
+             <h5 class="bold uppercase">Get In Touch</h5>
+             <hr/>
+             <ul class="inline-list">
+               <li><a class="icon icon-md icon-default fa-facebook" href="#"></a></li>
+               <li><a class="icon icon-md icon-default fa-google-plus" href="#"></a></li>
+               <li><a class="icon icon-md icon-default fa-rss" href="#"></a></li>
+               <li><a class="icon icon-md icon-default fa-pinterest" href="#"></a></li>
+               <li><a class="icon icon-md icon-default fa-linkedin" href="#"></a></li>
+             </ul>
+           </div>
+           <div class="col-md-3 col-sm-6 pull-lg-right">
+             <h5 class="bold uppercase">Online Support</h5>
+             <hr/>
+             <address>
+               <a href="callto:#">+ 8 800 757 43 92</a>
+               <dl class="samll">
+                 <dt>Ask Your questions by e-mail: </dt>
+                 <dd><a href="mailto:mail@demolink.org">mail@demolink.org</a></dd>
+               </dl>
+             </address>
+           </div>
+           <div class="col-md-3 col-sm-6 pull-lg-right">
+             <h5 class="bold uppercase">latest posts</h5>
+             <hr/>
+             <ul class="list">
+               <li>
+                 <p class="small">Eos et accusamus et iustom odgnissimos ducimus qui blap.</p>
+                 <time datetime="P5D">5 Days ago.</time>
+               </li>
+               <li>
+                 <p class="small">At accusamus et iustom odgnissimos ducimus qui blapraesentium. </p>
+                 <time datetime="P7D">7 Days ago.</time>
+               </li>
+             </ul>
+           </div>
+           <div class="col-md-3 col-sm-6 uppercase">
+             <h1 class="brand_name"> <a href="./">mariner</a> </h1>
+             <div class="copyright">
+               © <span id="copyright-year"></span>
+               <a href="index-5.html">| Privacy Policy</a>
+             </div>
+           </div>
+         </div>
+       </div>
+     </section>
+   </footer>
+</div>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
