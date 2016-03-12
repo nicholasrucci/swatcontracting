1459834ef0521a122356a661452017f154d32ed0
Stephen King <stephen@rocketmade.com>
2016-03-12 15:01:40 -0700

    required footer.php

--- index-5.php
+++ index-5.php
@@ -197,72 +197,7 @@
     <!--========================================================
                               FOOTER
     =========================================================-->
-    <footer class="text-center">
-        <section class="bg-primary well-xs banner text-md-left">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-9 wow fadeInLeft">
-                        <div class="banner-name">
-                            <p class="big">View our events calendar</p>
-                        </div>
-                    </div>
-                    <div class="col-md-3 wow fadeInRight">
-                        <time class="big" datetime="2015-01-01">2015</time>
-                        <span class="icon icon-primary-img"></span>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section class="well text-md-left bg-image-1">
-            <div class="container">
-                <div class="row flow-offset wow fadeInUp">
-                    <div class="col-md-3 col-sm-6 pull-lg-right">
-                        <h5 class="bold uppercase">Get In Touch</h5>
-                        <hr/>
-                        <ul class="inline-list">
-                            <li><a class="icon icon-md icon-default fa-facebook" href="#"></a></li>
-                            <li><a class="icon icon-md icon-default fa-google-plus" href="#"></a></li>
-                            <li><a class="icon icon-md icon-default fa-rss" href="#"></a></li>
-                            <li><a class="icon icon-md icon-default fa-pinterest" href="#"></a></li>
-                            <li><a class="icon icon-md icon-default fa-linkedin" href="#"></a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-3 col-sm-6 pull-lg-right">
-                        <h5 class="bold uppercase">Online Support</h5>
-                        <hr/>
-                        <address>
-                            <a href="callto:#">+ 8 800 757 43 92</a>
-                            <dl class="samll">
-                                <dt>Ask Your questions by e-mail: </dt>
-                                <dd><a href="mailto:mail@demolink.org">mail@demolink.org</a></dd>
-                            </dl>
-                        </address>
-                    </div>
-                    <div class="col-md-3 col-sm-6 pull-lg-right">
-                        <h5 class="bold uppercase">latest posts</h5>
-                        <hr/>
-                        <ul class="list">
-                            <li>
-                                <p class="small">Eos et accusamus et iustom odgnissimos ducimus qui blap.</p>
-                                <time datetime="P5D">5 Days ago.</time>
-                            </li>
-                            <li>
-                                <p class="small">At accusamus et iustom odgnissimos ducimus qui blapraesentium. </p>
-                                <time datetime="P7D">7 Days ago.</time>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="col-md-3 col-sm-6 uppercase">
-                        <h1 class="brand_name"> <a href="./">mariner</a> </h1>
-                        <div class="copyright">
-                            © <span id="copyright-year"></span>
-                            <a href="index-5.html">| Privacy Policy</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </footer>
+  <?php require_once('includes.footer.php'); ?>
 </div>
 <script src="js/script.js"></script>
 <!--coded by gap0n-->
